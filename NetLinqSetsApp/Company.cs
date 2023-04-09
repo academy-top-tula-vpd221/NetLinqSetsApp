a2543c5bc723d934e6b7b503238e523c27512263
Ефимов Максим <efimov_m@itstep.academy>
2023-04-09 16:06:58 +0300

    Добавьте файлы проекта.

---
+++ NetLinqSetsApp/Company.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetLinqSetsApp
+{
+    internal class Company
+    {
+        public string? Title { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title}";
+        }
+    }
+
+    class CompanyComparer : IEqualityComparer<Company>
+    {
+        public bool Equals(Company? x, Company? y)
+        {
+            return x.Title.Equals(y.Title);
+        }
+
+        public int GetHashCode([DisallowNull] Company obj)
+        {
+            return obj.Title.GetHashCode();
+        }
+    }
+}
---
+++ NetLinqSetsApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using NetLinqSetsApp;
+
+List<Company> companies1 = new()
+{
+    new(){ Title = "Yandex" },
+    new(){ Title = "Mail Group" },
+    new(){ Title = "Ozon" },
+    new(){ Title = "Avito" },
+    new(){ Title = "Google" },
+    new(){ Title = "Facebook" },
+    new(){ Title = "TikTok" },
+    new(){ Title = "Twitter" },
+};
+
+List<Company> companies2 = new()
+{
+    new(){ Title = "Facebook" },
+    new(){ Title = "Amazon" },
+    new(){ Title = "Mail Group" },
+    new(){ Title = "HotMail" },
+    new(){ Title = "Xiaomy" },
+    new(){ Title = "Samsung" },
+    new(){ Title = "Facebook" },
+    new(){ Title = "Youtube" },
+    new(){ Title = "Microsoft" },
+};
+
+foreach (var comp in companies1)
+    Console.WriteLine(comp);
+Console.WriteLine();
+
+foreach (var comp in companies2.Distinct(new CompanyComparer()))
+    Console.WriteLine(comp);
+Console.WriteLine();
+
+
+var compIntersect =  companies1.Intersect(companies2, new CompanyComparer());
+foreach(var comp in compIntersect)
+    Console.WriteLine(comp);
+Console.WriteLine();
+
+var compUnion = companies1.Union(companies2, new CompanyComparer());
+foreach (var comp in compUnion)
+    Console.WriteLine(comp);
+Console.WriteLine();
+
+var compExcept = companies1.Except(companies2, new CompanyComparer());
+foreach (var comp in compExcept)
+    Console.WriteLine(comp);
+Console.WriteLine();
+
